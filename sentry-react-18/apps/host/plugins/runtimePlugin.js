496a853285cebf5a8f79e4b18f96ac4c38dcd391
Rodrigo Yokota <ryok90@gmail.com>
2024-02-27 00:27:00 -0300

    feat: react + mf enhanced working

---
+++ sentry-react-18/apps/host/plugins/runtimePlugin.js
@@ -0,0 +1,25 @@
+function runtimePlugin() {
+  return {
+    name: 'custom-plugin-next-js-mf-9999',
+    async errorLoadRemote({ id, error, from, origin }) {
+      const pg = (await import('../src/components/error')).default
+
+      let mod;
+      if (from === 'build') {
+        mod = () => ({
+          __esModule: true,
+          default: pg,
+        });
+      } else {
+        mod = {
+          default: pg,
+        };
+      }
+
+      return mod;
+    },
+  };
+}
+
+module.exports = runtimePlugin;
+
---
+++ sentry-react-18/apps/remote/webpack.config.js
@@ -0,0 +1,67 @@
+const { ModuleFederationPlugin } = require('@module-federation/enhanced');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration & { devServer?: import('webpack-dev-server').Configuration }}
+ */
+const config = {
+  entry: './src/index',
+  mode: 'development',
+  cache: false,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3011,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+    publicPath: `auto`,
+    clean: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'remote',
+      filename: `remoteEntry.js`,
+      exposes: {
+        './table': './src/components/table/table',
+      },
+      shared: {},
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/assets/favicon.ico',
+    }),
+  ],
+};
+
+module.exports = config;
